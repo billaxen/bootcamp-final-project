c982635cad6fc407bb2b83ac13765d8668526d57
BILLA <116056817+billaxen@users.noreply.github.com>
2023-04-12 12:13:45 -0400

    added README

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,6 @@ import Home from "./Home";
 import MyRecipes from "./MyRecipes";
 import Favorites from "./Favorites";
 import AddRecipe from "./AddRecipe";
-import EditRecipe from "./EditRecipe";
 import Footer from "./Footer";
 import ProfilePage from "./ProfilePage";
 import RecipeList from "./RecipeList";
@@ -25,7 +24,6 @@ const App = () => {
           <Route path="/favorites" element={<Favorites />} />
           <Route path="/add-recipe" element={<AddRecipe />} />
           <Route path="/profile" element={<ProfilePage/>} />
-          <Route path="/edit-recipe/:id" element={<EditRecipe />} />
           <Route path="/recipe-list" element={<RecipeList />} />
           <Route path="/recipe/:recipeId" element={<RecipeCard />} />
           <Route path="/favrecipe/:recipeId" element={<FavRecipeCard />} />
--- client/src/EditRecipe.js
+++
@@ -1,9 +0,0 @@
-const EditRecipe = () => {
-return (
-    <div>
-        <h1>EditRecipe</h1>
-    </div>
-)
-};
-
-export default EditRecipe;
